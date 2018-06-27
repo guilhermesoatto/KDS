6bc5a847e2dd48d550636cd89384bec6525082bf
Gustavo <gutomoretti@gmail.com>
2018-06-27 16:44:51 -0300

    WebAPI
    Modelo do TNF Samples >> Scenarios >> BasicCrud
    Modelo inicial da Aplicação da Web API

---
+++ KDSApi/src/BasicCrud.Web/WebConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicCrud.Web
+{
+    public class WebConstants
+    {
+        public const string CustomerRouteName = "api/customers";
+        public const string ProductRouteName = "api/products";
+    }
+}
